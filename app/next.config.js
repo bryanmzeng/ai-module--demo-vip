7db3b1dc8a159b91aac759e5c094bc513a77110c
bzeng41912 <bryanzeng88@gmail.com>
2025-02-04 17:18:35 -0500

    pages setup

---
+++ app/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/ai-module--demo-vip',
+    assetPrefix: '/ai-module--demo-vip/',
+  }
+  
+  module.exports = nextConfig
